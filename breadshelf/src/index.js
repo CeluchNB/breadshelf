ea2624ee2efe2a7b6961f933c1ae8f0d42bb2bb7
CeluchNB <noah.celuch@gmail.com>
2019-07-07 15:11:42 -0400

    construction

--- breadshelf/src/index.js
+++ breadshelf/src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase';
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCYkBwLvzE-hWo35smeIj2Y1QuraSXJxCQ",
+    authDomain: "breadshelf.firebaseapp.com",
+    databaseURL: "https://breadshelf.firebaseio.com",
+    projectId: "breadshelf",
+    storageBucket: "",
+    messagingSenderId: "93936832869",
+    appId: "1:93936832869:web:a4cb8da2ad81df9b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

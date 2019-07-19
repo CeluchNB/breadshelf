171d6573e6d3b1c42d240e64b9ea75ec27767d27
CeluchNB <noah.celuch@gmail.com>
2019-07-18 23:34:39 -0400

    refactored create account to use firebase consumer

--- breadshelf/src/CreateAccountPage/index.js
+++ breadshelf/src/CreateAccountPage/index.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import fire, { auth } from './../Firebase/firebase.js';
+import { Link, withRouter } from 'react-router-dom';
+import FirebaseContext, { withFirebase } from './../Firebase';
 import * as ROUTES from '../constants/routes.js';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { MuiThemeProvider } from '@material-ui/core/styles';
-import theme from './../theme/theme';
+import theme from './../theme/theme.js';
 import './index.css';
 
 const AdapterLink = React.forwardRef((props, ref) => <Link innerRef={ref} {...props} />);
 
-class CreateAccountPage extends Component {
+const CreateAccountPage = () => (
+    <MuiThemeProvider theme={theme}>
+        <div className="CreateAccountPage">
+            <h1 className="Header">create account</h1>
+            <CreateAccountForm />
+        </div>
+    </MuiThemeProvider>
+);
+
+class CreateAccountFormBase extends Component {
     constructor(props) {
         super(props);
         
@@ -25,7 +34,6 @@ class CreateAccountPage extends Component {
             lastNameStyle: { width: "15%" },
             largeFieldStyle: { width: "32%" }
         }
-        console.log(window.innerWidth);
     }
 
     createAccount() {
@@ -33,7 +41,6 @@ class CreateAccountPage extends Component {
     }
 
     updateStyles() {
-        console.log(window.innerWidth);
         if(window.innerWidth > 800 ) {
             this.setState({
                 firstNameStyle: { width: "15%", marginRight: "2%" },
@@ -60,142 +67,141 @@ class CreateAccountPage extends Component {
 
     render() {
         return (
-            <MuiThemeProvider theme={theme}>
-                <div className="CreateAccountPage">
-                    <h1 className="Header">create account</h1>
-                    <TextField
-                        id="firstname-input"
-                        label="First Name"
-                        type="text"
-                        name="firstname"
-                        autoComplete="firstname"
-                        margin="normal"
-                        variant="outlined"
-                        autoFocus={true}
-                        style={ this.state.firstNameStyle }
-                        required
-                        onChange={
-                            (event) => { 
-                                this.setState({ firstname: event.target.value });
-                            }
+            <div className="CreateAccountForm">
+                <TextField
+                    id="firstname-input"
+                    label="First Name"
+                    type="text"
+                    name="firstname"
+                    autoComplete="firstname"
+                    margin="normal"
+                    variant="outlined"
+                    autoFocus={true}
+                    style={ this.state.firstNameStyle }
+                    required
+                    onChange={
+                        (event) => { 
+                            this.setState({ firstname: event.target.value });
                         }
-                        value={this.state.firstname}
-                    />
+                    }
+                    value={this.state.firstname}
+                />
+                <TextField
+                    id="lastname-input"
+                    label="Last Name"
+                    type="text"
+                    name="lastname"
+                    autoComplete="lastname"
+                    margin="normal"
+                    variant="outlined"
+                    style={ this.state.lastNameStyle }
+                    required
+                    onChange={
+                        (event) => {
+                            this.setState({ lastname: event.target.value });
+                        }
+                    }
+                    value={this.state.lastname}
+                />
+                <div>
                     <TextField
-                        id="lastname-input"
-                        label="Last Name"
-                        type="text"
-                        name="lastname"
-                        autoComplete="lastname"
+                        id="email-input"
+                        label="Email"
+                        type="email"
+                        name="email"
+                        autoComplete="email"
                         margin="normal"
                         variant="outlined"
-                        style={ this.state.lastNameStyle }
+                        style={ this.state.largeFieldStyle }
                         required
                         onChange={
                             (event) => {
-                                this.setState({ lastname: event.target.value });
+                                this.setState({ email: event.target.value });
                             }
                         }
-                        value={this.state.lastname}
+                        value={this.state.email}
                     />
-                    <div>
-                        <TextField
-                            id="email-input"
-                            label="Email"
-                            type="email"
-                            name="email"
-                            autoComplete="email"
-                            margin="normal"
-                            variant="outlined"
-                            style={ this.state.largeFieldStyle }
-                            required
-                            onChange={
-                                (event) => {
-                                    this.setState({ email: event.target.value });
-                                }
+                </div>
+                <div>
+                    <TextField
+                        id="username=input"
+                        label="Username"
+                        type="text"
+                        name="username"
+                        autoComplete="username"
+                        margin="normal"
+                        variant="outlined"
+                        style={ this.state.largeFieldStyle }
+                        required
+                        onChange={
+                            (event) => {
+                                this.setState({ username: event.target.value });
                             }
-                            value={this.state.email}
-                        />
+                        }
+                        value={this.state.username}
+                    />
+                </div>
+                <div>
+                    <TextField
+                        id="password-input"
+                        label="Password"
+                        type="password"
+                        name="password"
+                        autoComplete="password"
+                        margin="normal"
+                        variant="outlined"
+                        style={ this.state.largeFieldStyle }
+                        required
+                        onChange={
+                            (event) => {
+                                this.setState({ password: event.target.value });
+                            }
+                        }
+                        value={this.state.password}
+                    />
+                </div>
+                <div>
+                    <TextField
+                        id="confirm-password-input"
+                        label="Confirm Password"
+                        type="password"
+                        name="confirm-password"
+                        autoComplete="confirmpassword"
+                        margin="normal"
+                        variant="outlined"
+                        style={ this.state.largeFieldStyle }
+                        required
+                        onChange={
+                            (event) => {
+                                this.setState({ confirmPassword: event.target.value });
+                            }
+                        }
+                        value={this.state.confirmPassword}
+                    />
+                </div>
+                <div className="CreateAccountButtons">
+                    <div>
+                        <Button
+                            variant="contained"
+                            color="primary">
+                            Create
+                        </Button>
                     </div>
                     <div>
-                        <TextField
-                            id="username=input"
-                            label="Username"
-                            type="text"
-                            name="username"
-                            autoComplete="username"
-                            margin="normal"
-                            variant="outlined"
-                            style={ this.state.largeFieldStyle }
-                            required
-                            onChange={
-                                (event) => {
-                                    this.setState({ username: event.target.value });
-                                }
-                            }
-                            value={this.state.username}
-                        />
-                    </div>
-                    <div>
-                        <TextField
-                            id="password-input"
-                            label="Password"
-                            type="password"
-                            name="password"
-                            autoComplete="password"
-                            margin="normal"
-                            variant="outlined"
-                            style={ this.state.largeFieldStyle }
-                            required
-                            onChange={
-                                (event) => {
-                                    this.setState({ password: event.target.value });
-                                }
-                            }
-                            value={this.state.password}
-                        />
-                    </div>
-                    <div>
-                        <TextField
-                            id="confirm-password-input"
-                            label="Confirm Password"
-                            type="password"
-                            name="confirm-password"
-                            autoComplete="confirmpassword"
-                            margin="normal"
-                            variant="outlined"
-                            style={ this.state.largeFieldStyle }
-                            required
-                            onChange={
-                                (event) => {
-                                    this.setState({ confirmPassword: event.target.value });
-                                }
-                            }
-                            value={this.state.confirmPassword}
-                        />
-                    </div>
-                    <div className="CreateAccountButtons">
-                        <div>
-                            <Button
-                                variant="contained"
-                                color="primary">
-                                Create
-                            </Button>
-                        </div>
-                        <div>
-                            <Button
-                                variant="text"
-                                color="primary"
-                                component={AdapterLink}
-                                to={ROUTES.LANDING}>
-                                Cancel
-                            </Button>
-                        </div>
+                        <Button
+                            variant="text"
+                            color="primary"
+                            component={AdapterLink}
+                            to={ROUTES.LANDING}>
+                            Cancel
+                        </Button>
                     </div>
                 </div>
-            </MuiThemeProvider>
+            </div>
         );
     }
 }
+
+const CreateAccountForm = withRouter(withFirebase(CreateAccountFormBase));
 
 export default CreateAccountPage;
